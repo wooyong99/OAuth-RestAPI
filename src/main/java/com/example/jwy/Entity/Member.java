9c5b7df9cde0022938f5766b8c7ae8acc7f2b041
wooyong99 <wulovesk@naver.com>
2023-09-22 09:03:43 +0900

    Setting

---
+++ src/main/java/com/example/jwy/Entity/Member.java
@@ -0,0 +1,21 @@
+package com.example.jwy.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String nickname;
+}
---
+++ src/test/java/com/example/jwy/JwyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
